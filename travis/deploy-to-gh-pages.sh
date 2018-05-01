03e51a10eb0d88258b7a7dcd32c4943d7cf606b2
Travis CI <nobody@nobody.org>
2018-05-01 19:45:48 +0800

    Fixed travis not deploying

--- travis/deploy-to-gh-pages.sh
+++ travis/deploy-to-gh-pages.sh
@@ -10,15 +10,11 @@ git config --global user.name "Travis CI"
 rm -rf www/
 
 # build
-echo Building
 ionic build --prod
 
 # deploy
-echo Deploying
 cd www
 git init
 git add .
-echo Committing
 git commit -m "Deploy to Github Pages"
-echo Pushing
-git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
