0cd61ab223c4923becc5c8c509f915a06f2f1067
Sheen Carbellido <Sheen.Carbellido@ddp.kyocera.com>
2018-05-25 18:39:49 +0800

    Update UI and mockups

---
+++ src/pages/models/hangul.characters.js
@@ -0,0 +1,136 @@
+{
+    hangul: {
+        consonants: [
+            {
+                "sound": 'g/k',
+                "character": 'ㄱ' 
+            }, {
+                "sound": 'n',
+                "character": 'ㄴ' 
+            }, {
+                "sound": 'd/t',
+                "character": 'ㄷ' 
+            }, {
+                "sound": 'r/l',
+                "character": 'ㄹ' 
+            }, {
+                "sound": 'm',
+                "character": 'ㅁ' 
+            }, {
+                "sound": 'b/p',
+                "character": 'ㅂ' 
+            }, {
+                "sound": 's',
+                "character": 'ㅅ' 
+            }, {
+                "sound": '-/ng',
+                "character": 'ㅇ' 
+            }, {
+                "sound": 'j',
+                "character": 'ㅈ' 
+            }
+        ] 
+        tenseConsonants: [
+            {
+                "sound": 'kk/k',
+                "character": 'ㄲ' 
+            }, {
+                "sound": 'tt',
+                "character": 'ㄸ' 
+            }, {
+                "sound": 'pp',
+                "character": 'ㅃ' 
+            }, {
+                "sound": 'ss/t',
+                "character": 'ㅆ' 
+            }, {
+                "sound": 'jj',
+                "character": 'ㅉ' 
+            }
+        ]
+        aspiratedConsonants: [
+            {
+                "sound": 'ch/t',
+                "character": 'ㅊ' 
+            }, {
+                "sound": 'k',
+                "character": 'ㅋ' 
+            }, {
+                "sound": 't',
+                "character": 'ㅌ' 
+            }, {
+                "sound": 'p',
+                "character": 'ㅍ' 
+            }, {
+                "sound": 'h',
+                "character": 'ㅎ' 
+            }
+        ]
+        vowels: [
+            {
+                "sound": 'a',
+                "character": 'ㅏ' 
+            }, {
+                "sound": 'ae',
+                "character": 'ㅐ' 
+            }, {
+                "sound": 'ya',
+                "character": 'ㅑ' 
+            }, {
+                "sound": 'yae',
+                "character": 'ㅒ' 
+            }, {
+                "sound": 'eo',
+                "character": 'ㅓ' 
+            }, {
+                "sound": 'e',
+                "character": 'ㅔ' 
+            }, {
+                "sound": 'yeo',
+                "character": 'ㅕ' 
+            }, {
+                "sound": 'ye',
+                "character": 'ㅖ' 
+            }, {
+                "sound": 'o',
+                "character": 'ㅗ' 
+            }, {
+                "sound": 'wa',
+                "character": 'ㅘ' 
+            }, {
+                "sound": 'wae',
+                "character": 'ㅙ' 
+            }, {
+                "sound": 'oe',
+                "character": 'ㅚ' 
+            }, {
+                "sound": 'yo',
+                "character": 'ㅛ' 
+            }, {
+                "sound": 'u',
+                "character": 'ㅜ' 
+            }, {
+                "sound": 'wo',
+                "character": 'ㅝ' 
+            }, {
+                "sound": 'we',
+                "character": 'ㅞ' 
+            }, {
+                "sound": 'yu',
+                "character": 'ㅠ' 
+            }, {
+                "sound": 'wi',
+                "character": 'ㅟ' 
+            }, {
+                "sound": 'eu',
+                "character": 'ㅡ' 
+            }, {
+                "sound": 'ui',
+                "character": 'ㅢ' 
+            }, {
+                "sound": 'i',
+                "character": 'ㅣ' 
+            }
+        ]
+    }
+}
